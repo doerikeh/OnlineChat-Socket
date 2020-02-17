d1269ceb6a581ffc35d6e07999d5b6dd75963467
doerikeh1 <doerikeh1@gmail.com>
2020-02-17 15:16:56 +0700

    create Ui Web

--- web/app.py
+++ web/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, url_for, redirect
 
-NAME_KEY = "name"
+
 
 
 app = Flask(__name__)
 app.secret_key = "Halo"
+
+NAME_KEY = "name"
 
 @app.route("/login")
 def login():
@@ -19,10 +21,13 @@ def logout():
 @app.route('/')
 @app.route('/home')
 def home():
-    if NAME_KEY not session:
-        return redirect(url_for("login"))
-    name = session[NAME_KEY]
+    # if NAME_KEY not in session:
+    #     return redirect(url_for("login"))
+    # name = session[NAME_KEY]
     return render_template("index.html")
+
+def run():
+    print("clicked")
 
 if __name__ == "__main__":
     app.run(debug=True)
